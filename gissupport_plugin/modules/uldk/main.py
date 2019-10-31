4445adbf9d4a967e8a67184cb3fbc9502f7587cc
Kamil Kozik <kamil.kozik@gis-support.pl>
2019-10-31 11:05:47 +0100

    porządki w uldk/main.py; bardziej eleganckie sprawdzania, czy wms został usunięty

--- gissupport_plugin/modules/uldk/main.py
+++ gissupport_plugin/modules/uldk/main.py
@@ -81,100 +81,6 @@ class Main:
         self.wms_lpis_layer = None
         self.dockwidget.button_wms_lpis.clicked.connect(self.add_wms_lpis)
         self.module_wms_lpis_initialized = True
-
-    # def add_action(
-    #     self,
-    #     name,
-    #     icon_path,
-    #     text,
-    #     callback,
-    #     enabled_flag=True,
-    #     add_to_menu=True,
-    #     add_to_toolbar=True,
-    #     status_tip=None,
-    #     whats_this=None,
-    #     parent=None,
-    #     checkable=False):
-
-    #     icon = QIcon(icon_path)
-    #     action = QAction(icon, text, parent)
-    #     action.triggered.connect(callback)
-    #     action.setEnabled(enabled_flag)
-    #     action.setCheckable(checkable)
-
-    #     if status_tip is not None:
-    #         action.setStatusTip(status_tip)
-
-    #     if whats_this is not None:
-    #         action.setWhatsThis(whats_this)
-
-    #     if add_to_toolbar:
-    #         self.toolbar.addAction(action)
-
-    #     if add_to_menu:
-    #         self.iface.addPluginToMenu(
-    #             self.menu,
-    #             action)
-
-    #     self.toolbar_buttons[name] = action
-
-    #     return action
-
-    # def initGui(self):
-
-    #     base_directory = ':/plugins/plugin/'
-    #     self.add_action(
-    #         "main",
-    #         os.path.join(base_directory, "logo_thumb.png"),
-    #         text=self.tr(PLUGIN_NAME),
-    #         callback=self.run,
-    #         parent=self.iface.mainWindow())
-
-    #     action_map_point_search = self.add_action(
-    #         "plot_getter",
-    #         self.module_map_point_search.get_icon(),
-    #         text = "Identifykacja ULDK",
-    #         callback = lambda state : self.module_map_point_search.toggle(not state),
-    #         parent = self.iface.mainWindow(),
-    #         checkable = True
-    #     )    
-    #     self.module_map_point_search.deactivated.connect(lambda: action_map_point_search.setChecked(False))
-
-    # def run(self):
-
-    #     if not self.pluginIsActive:
-    #         self.pluginIsActive = True
-    #         if self.dockwidget == None:
-    #             self.dockwidget = wyszukiwarkaDzialekDockWidget()
-
-    #         # self.dockwidget.closingPlugin.connect(self.onClosePlugin)
-    #         # self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dockwidget)
-    #         # self.dockwidget.show()
-
-    #         if self.module_teryt_search is None:
-    #             result_collector_factory = lambda parent, target_layer: ResultCollectorMultiple(self, target_layer)
-    #             self.module_teryt_search = TerytSearch(self,
-    #                 self.dockwidget.tab_teryt_search_layout,
-    #                 self.teryt_search_result_collector,
-    #                 result_collector_factory,
-    #                 ResultCollectorMultiple.default_layer_factory)
-
-    #         if self.module_csv_import is None:
-    #             result_collector_factory = lambda parent, target_layer: ResultCollectorMultiple(self, target_layer)
-    #             self.module_csv_import = CSVImport(self,
-    #                 self.dockwidget.tab_import_csv_layout, 
-    #                 result_collector_factory,
-    #                 ResultCollectorMultiple.default_layer_factory)
-
-    #         if self.module_point_layer_import is None:
-    #             self.module_point_layer_import = PointLayerImport(
-    #                 self,
-    #                 self.dockwidget.tab_import_layer_point_layout)
-
-    #         if not self.module_wms_kieg_initialized:
-    #             self.dockwidget.button_wms_kieg.clicked.connect(self.add_wms_kieg)
-    #             self.project.layersRemoved.connect( lambda layers : self.dockwidget.button_wms_kieg.setEnabled(True) if filter(lambda layer: layer.customProperty("ULDK") == "wms_kieg_layer", layers) else lambda : None)
-    #             self.module_wms_kieg_initialized = True
         
     def add_wms_kieg(self):
         
@@ -192,16 +98,10 @@ class Main:
             layer.setCustomProperty("ULDK", "wms_kieg_layer")
             self.wms_kieg_layer = layer
             self.project.addMapLayer(self.wms_kieg_layer)
-            self.project.layerWillBeRemoved.connect(self.before_wms_kieg_layer_removed)
+            self.project.layerWillBeRemoved[QgsMapLayer].connect(self.before_layer_removed)
             self.dockwidget.button_wms_kieg.setEnabled(False)
         else:
             self.canvas.refresh()
-
-    def before_wms_kieg_layer_removed(self, layer_id):
-        layer = self.project.layerStore().mapLayer(layer_id)
-        if layer.customProperty("ULDK") == "wms_kieg_layer":
-            self.wms_kieg_layer = None
-            self.dockwidget.button_wms_kieg.setEnabled(True)
 
     def add_wms_lpis(self):
         
@@ -213,13 +113,16 @@ class Main:
             layer.setScaleBasedVisibility(True)
             self.wms_lpis_layer = layer
             self.project.addMapLayer(self.wms_lpis_layer)
-            self.project.layerWillBeRemoved.connect(self.before_wms_lpis_layer_removed)
+            self.project.layerWillBeRemoved[QgsMapLayer].connect(self.before_layer_removed)
             self.dockwidget.button_wms_lpis.setEnabled(False)
         else:
             self.canvas.refresh()
 
-    def before_wms_lpis_layer_removed(self, layer_id):
-        layer = self.project.layerStore().mapLayer(layer_id)
+    def before_layer_removed(self, layer):
         if layer.customProperty("ULDK") == "wms_lpis_layer":
             self.wms_lpis_layer = None
             self.dockwidget.button_wms_lpis.setEnabled(True)
+        if layer.customProperty("ULDK") == "wms_kieg_layer":
+            self.wms_kieg_layer = None
+            self.dockwidget.button_wms_kieg.setEnabled(True)
+

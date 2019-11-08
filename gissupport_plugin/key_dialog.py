c389242e4a347a94a7e6a13c34e0a2fa1979994f
Hubert Zakościelny <hubert.zakoscielny@gis-support.pl>
2019-11-08 12:00:37 +0100

    Sprawdzanie długości znaków klucza GIS Support

--- gissupport_plugin/key_dialog.py
+++ gissupport_plugin/key_dialog.py
@@ -2,7 +2,7 @@ import os
 
 from PyQt5 import uic
 from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMessageBox
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'key_dialog.ui'))
@@ -20,5 +20,12 @@ class GisSupportPluginDialog(QDialog, FORM_CLASS):
 
     def saveKey(self):
         key = self.keyLineEdit.text().strip()
+        if len(key) != 43:
+            messageBox = QMessageBox.warning(
+                None,
+                "Wtyczka GIS Support",
+                "Wpisz poprawny klucz GIS Support"
+            )
+            return
         QSettings().setValue('gissupport/api/key', key)
         self.hide()

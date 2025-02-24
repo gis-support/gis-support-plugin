56c174252e729a20d40e0f0a1c4520270100ed9e
mateuszrydzik <panmisterek@outlook.com>
2025-02-24 09:55:18 +0100

    SRVS-1829, walidacja uprawnien uzytkownika do edycji warstw

--- gissupport_plugin/tools/gisbox_connection.py
+++ gissupport_plugin/tools/gisbox_connection.py
@@ -28,6 +28,8 @@ class GisboxConnection(QObject, Logger):
 
         self.twoFaDialog = None
 
+        self.current_user = None
+
     @classmethod
     def _exec_callback(cls, uuid_: str):
         reply, callback = cls.QUEUE[uuid_]
@@ -107,19 +109,42 @@ class GisboxConnection(QObject, Logger):
             self.log("Połączono")
             self.on_connect.emit(True)
             self.is_connected = True
+            self.get_current_user()
             return True
         self.on_disconnect.emit()
         return False
 
+    def get_current_user(self):
+
+        if not self.current_user:
+
+            request = self._createRequest('/api/users/current_user')
+            response = self.MANAGER.blockingGet(request, forceRefresh=True)
+
+            response_data = json.loads(bytearray(response.content()))
+            data = response_data['data']
+
+            permissions = data['permissions']
+            layers_dict = {l["layer_id"]: l for l in permissions['layers']}
+            modules_dict = {m["module_name"]: m for m in permissions['modules']}
+
+            data['permissions']['layers'] = layers_dict
+            data['permissions']['modules'] = modules_dict
+
+            self.current_user = data
+
+
     def disconnect(self):
 
-        request = self._createRequest('/api/logout')
-        request.setRawHeader(b'X-Access-Token', bytes(self.token.encode()))
-        self.MANAGER.blockingGet(request)
+        if self.token:
+            request = self._createRequest('/api/logout')
+            request.setRawHeader(b'X-Access-Token', bytes(self.token.encode()))
+            self.MANAGER.blockingGet(request)
         self.log("Rozłączono")
         self.on_disconnect.emit()
         self.is_connected = False
         self.token = None
+        self.current_user = None
         return True
 
     def _createRequest(self, endpoint: str, content_type: str = 'application/json',
@@ -128,7 +153,7 @@ class GisboxConnection(QObject, Logger):
         request = QNetworkRequest(QUrl(host + endpoint))
         request.setHeader(QNetworkRequest.ContentTypeHeader, content_type)
         request.setRawHeader(b'X-User-Agent', b'qgis_gs')
-        if with_token:
+        if with_token and self.token:
             request.setRawHeader(b'X-Access-Token', bytes(self.token.encode()))
 
         return request

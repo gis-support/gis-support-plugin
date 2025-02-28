69f157d14bc4d1e7de8c6e7632bb573fdcca3357
mateusz <62845725+mateuszrydzik@users.noreply.github.com>
2025-02-28 13:56:26 +0100

    SRVS-1820, wyłączenie paneli BDOT10k przy braku połączenia z Internetem (#77)
    * SRVS-1820, wyłączenie paneli BDOT10k przy braku połączenia z Internetem
    
    * SRVS-1820, weryfikacja połączenia z internetem przy pobieraniu BDOT10k dla powiatu, poprawa logiki kodu dla weryfikacji połączenia z Databox
    
    * SRVS-1820, sprawdzanie połączenia z geoportalem i wskazania ściezki zapisu dla powiatu i klas

--- gissupport_plugin/tools/requests.py
+++ gissupport_plugin/tools/requests.py
@@ -46,7 +46,7 @@ class NetworkHandler(QObject):
             request = QNetworkRequest(QUrl(url))
             
             reply = self.network_manager.get(request)
-            reply.downloadProgress.connect( lambda recv, total: self.downloadProgress.emit( int(100*recv/total) ) )
+            reply.downloadProgress.connect( lambda recv, total: self.downloadProgress.emit(self.set_progress(recv, total)))
             reply.finished.connect(lambda: self.handle_response(reply, retry_callback, reply_only))
             return reply
 
@@ -73,7 +73,7 @@ class NetworkHandler(QObject):
             if databox:
                 request.setHeader(QNetworkRequest.ContentTypeHeader, "application/json")
             reply = self.network_manager.post(request, body)
-            reply.downloadProgress.connect( lambda recv, total: self.downloadProgress.emit( int(100*recv/total) ) )
+            reply.downloadProgress.connect( lambda recv, total: self.downloadProgress.emit(self.set_progress(recv, total)))
             reply.finished.connect(lambda: self.handle_response(reply, retry_callback, reply_only))
             return reply
 
@@ -92,3 +92,9 @@ class NetworkHandler(QObject):
             app.processEvents()
 
         return self.result
+
+    def set_progress(self, recv, total):
+
+        if total == 0:
+            return 0
+        return int(100*recv/total)

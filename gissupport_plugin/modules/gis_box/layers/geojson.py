a49248bc84aa5dda183acdba4ebe977dcccc1915
mateuszrydzik <panmisterek@outlook.com>
2025-03-19 11:55:46 +0100

    SRVS-1926, poprawienie parsowania obiektów bez geometrii

--- gissupport_plugin/modules/gis_box/layers/geojson.py
+++ gissupport_plugin/modules/gis_box/layers/geojson.py
@@ -9,6 +9,8 @@ def geojson2geom(geojson: dict) -> QgsGeometry:
     """ Konwersja geometrii GeoJSON na QgsGeometry """
     # Najpierw musimy uzyskać format pośredni, który obsługuje QGIS
     # WKB jest najszybsze
+    if not geojson:
+        geojson = "null" # od QGIS 3.40 wywala błąd w momencie gdy geometria jest typu None
     wkb = CreateGeometryFromJson(json.dumps(geojson)).ExportToWkb()
     # Stworzenie nowej geometrii QgsGeometry
     geometry = QgsGeometry()

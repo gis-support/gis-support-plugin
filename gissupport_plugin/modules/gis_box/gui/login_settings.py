dc6bd74f0060ecd9e820f7c099a5eafce52ef8bd
Kamil Iwaniuk <kamil.iwaniuk@gis-support.pl>
2025-06-16 14:05:09 +0200

    DEV-12538

--- gissupport_plugin/modules/gis_box/gui/login_settings.py
+++ gissupport_plugin/modules/gis_box/gui/login_settings.py
@@ -19,7 +19,7 @@ class LoginSettingsDialog(QDialog, FORM_CLASS):
         settings.beginGroup('gissupport/gisbox_connection')
         settings.setValue('user', settings.value('user', 'DEMO'))
         settings.setValue('pass', settings.value('pass', 'JesliChceszEdytowacNapiszDoNas:)'))
-        settings.setValue('host', settings.value('host', 'https://demo.gisbox.pl'))
+        settings.setValue('host', settings.value('host', 'demo.gisbox.pl'))
         
         self.leLogin.setText(settings.value('user', 'DEMO'))
         self.leLogin.textChanged.connect(
@@ -27,6 +27,6 @@ class LoginSettingsDialog(QDialog, FORM_CLASS):
         self.lePassword.setText(settings.value('pass', 'JesliChceszEdytowacNapiszDoNas:)'))
         self.lePassword.textChanged.connect(
             lambda text: settings.setValue('pass', text))
-        self.leHost.setText(settings.value('host', 'https://demo.gisbox.pl'))
+        self.leHost.setText(settings.value('host', 'demo.gisbox.pl'))
         self.leHost.textChanged.connect(
             lambda text: settings.setValue('host', text))

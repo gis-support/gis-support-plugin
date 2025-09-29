3f0c52922f55ccaed3e5aecc929c98e66370e004
Kamil Iwaniuk <kamil.iwaniuk@gis-support.pl>
2025-09-29 11:28:48 +0200

    SRVS-2392

--- gissupport_plugin/modules/uldk/uldk/resultcollector.py
+++ gissupport_plugin/modules/uldk/uldk/resultcollector.py
@@ -11,7 +11,7 @@ PLOTS_LAYER_DEFAULT_FIELDS = [
     QgsField("arkusz", QVariant.String),
     QgsField("nr_dzialki", QVariant.String),
     QgsField("teryt", QVariant.String),
-    QgsField("pow_m2", QVariant.String),
+    QgsField("pow_m2", QVariant.Double, prec=2),
 ]
 
 

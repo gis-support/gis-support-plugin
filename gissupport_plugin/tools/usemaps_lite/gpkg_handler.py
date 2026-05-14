452d2f7c1c3a8728e4a6bfff05ea5319fcdfd718
Mateusz Gryzio <mateuszgryzio@outlook.com>
2026-05-14 13:48:29 +0200

    SRVS-2875

--- gissupport_plugin/tools/usemaps_lite/gpkg_handler.py
+++ gissupport_plugin/tools/usemaps_lite/gpkg_handler.py
@@ -58,7 +58,7 @@ class GpkgHandler:
         options = QgsVectorFileWriter.SaveVectorOptions()
         options.driverName = "GPKG"
         options.fileEncoding = "UTF-8"
-        options.overrideGeometryType = QgsWkbTypes.dropM(QgsWkbTypes.dropZ(source_layer.wkbType()))
+        options.overrideGeometryType = QgsWkbTypes.flatType(source_layer.wkbType())
 
         transform_context = QgsProject.instance().transformContext()
 
@@ -86,9 +86,14 @@ class GpkgHandler:
             ))
             temp_layer.commitChanges()
 
-        temp_path = os.path.join(tempfile.gettempdir(), f"{''.join(c for c in layer.name() if c.isalnum() or c in (' ', '_', '-')).strip()}.gpkg")
+        temp_path = os.path.join(
+            tempfile.gettempdir(),
+            f"{''.join(c for c in layer.name() if c.isalnum() or c in (' ', '_', '-')).strip()}.gpkg"
+            )
+
         options = QgsVectorFileWriter.SaveVectorOptions()
-        options.overrideGeometryType = QgsWkbTypes.dropM(QgsWkbTypes.dropZ(temp_layer.wkbType()))
+        options.overrideGeometryType = QgsWkbTypes.flatType(temp_layer.wkbType())
+
         if QgsVectorFileWriter.writeAsVectorFormatV3(
             temp_layer,
             temp_path,

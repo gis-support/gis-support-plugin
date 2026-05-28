16f069465d03c546eb3df4068fff1bf2d4d48f7e
Mateusz Gryzio <mateuszgryzio@outlook.com>
2026-05-28 14:14:42 +0200

    DEV-14509: Usunięcie biblioteki defusedxml

--- gissupport_plugin/tools/capabilities.py
+++ gissupport_plugin/tools/capabilities.py
@@ -9,45 +9,54 @@ from typing import Union
 from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit
 
 try:
-  import defusedxml.ElementTree as et
-  from defusedxml.common import EntitiesForbidden
+    import defusedxml.ElementTree as et
+    from defusedxml.common import EntitiesForbidden
 except (ModuleNotFoundError, ImportError):
-  import gissupport_plugin.lib.defusedxml.ElementTree as et
-  from gissupport_plugin.lib.defusedxml.common import EntitiesForbidden
+    import xml.etree.ElementTree as et # nosec B405
+
+    class EntitiesForbidden(Exception):
+        """Pusta klasa wyjątku dla zachowania kompatybilności bloku try-except, gdy brak defusedxml."""
+        pass
 
 class CapabilitiesConnectionException(Exception):
     def __init__(self, code: int, *args, **kwargs):
             self.code = code
             super().__init__(*args, **kwargs)
 
-def get_capabilities(url: str, type: str) -> Union[WebMapService, WebFeatureService]:
+def get_capabilities(url: str, service_type: str) -> Union[WebMapService, WebFeatureService, WebMapTileService]:
     manager = QgsNetworkAccessManager()
 
     scheme, netloc, path, params, _ = urlsplit(url)
     query_params = parse_qs(params)
-    query_params["service"] = [f"{type}"]
+    query_params["service"] = [f"{service_type}"]
     query_params["request"] = ["GetCapabilities"]
     new_params = urlencode(query_params, doseq=True)
 
     request_url = urlunsplit((scheme, netloc, path, new_params, _))
     request = QNetworkRequest(QUrl(request_url))
     reply = manager.blockingGet(request)
-    
+
     if reply.error() != QNetworkReply.NetworkError.NoError or not reply.content():
         raise CapabilitiesConnectionException(code=reply.error())
 
-    content = reply.content()
-    data = content.data()
+    xml_data = reply.content().data()
+
     try:
-        xml = et.fromstring(data)
+        xml = et.fromstring(xml_data) # nosec B314
     except EntitiesForbidden as e:
         print_exc()
         raise CapabilitiesConnectionException(code=409, message=f"Dokument XML pobrany z adresu {url} zawiera potencjalnie niebezpieczne fragmenty i został zablokowany") from e
-    version =  xml.attrib.get("version")
-    
-    if type == "WMS":
-        return WebMapService(url="", version=version, xml=data)
-    elif type == "WFS":
-        return WebFeatureService(url="", version=version, xml=data)
-    elif type == "WMTS":
-        return WebMapTileService(url="", version=version, xml=data)
+    except et.ParseError as e:
+        print_exc()
+        raise CapabilitiesConnectionException(code=500, message=f"Błąd parsowania XML z adresu {url}") from e
+
+    version = xml.attrib.get("version")
+
+    if service_type == "WMS":
+        return WebMapService(url="", version=version, xml=xml_data)
+
+    elif service_type == "WFS":
+        return WebFeatureService(url="", version=version, xml=xml_data)
+
+    elif service_type == "WMTS":
+        return WebMapTileService(url="", version=version, xml=xml_data)
